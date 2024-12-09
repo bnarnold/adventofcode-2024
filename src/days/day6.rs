1c0ccd21eb160677e3535d86ce5059b60c4c9cb7
Bertram Arnold <bertram.arnold@gmail.com>
2024-12-10 00:21:26 +0100

    day 6 part 2

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -2,11 +2,70 @@ use std::collections::{BTreeMap, BTreeSet, HashSet};
 
 use crate::util::prelude::*;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 struct Map {
     obstacles: HashSet<(usize, usize)>,
     width: usize,
     height: usize,
+}
+
+impl Map {
+    fn steps_till_escape(&self, mut pos: (usize, usize)) -> Option<HashSet<(usize, usize)>> {
+        #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+        enum Dir {
+            North,
+            East,
+            South,
+            West,
+        }
+        let mut dir = Dir::North;
+        let mut visited: HashSet<_> = std::iter::once(pos).collect();
+        let mut visited_dir: HashSet<_> = std::iter::once((pos, dir)).collect();
+        loop {
+            let next_pos = match dir {
+                Dir::North => {
+                    let Some(y) = pos.1.checked_sub(1) else {
+                        return Some(visited);
+                    };
+                    (pos.0, y)
+                }
+                Dir::East => {
+                    let x = pos.0 + 1;
+                    if x >= self.width {
+                        return Some(visited);
+                    }
+                    (x, pos.1)
+                }
+                Dir::South => {
+                    let y = pos.1 + 1;
+                    if y >= self.height {
+                        return Some(visited);
+                    }
+                    (pos.0, y)
+                }
+                Dir::West => {
+                    let Some(x) = pos.0.checked_sub(1) else {
+                        return Some(visited);
+                    };
+                    (x, pos.1)
+                }
+            };
+            if self.obstacles.contains(&next_pos) {
+                dir = match dir {
+                    Dir::North => Dir::East,
+                    Dir::East => Dir::South,
+                    Dir::South => Dir::West,
+                    Dir::West => Dir::North,
+                }
+            } else {
+                visited.insert(next_pos);
+                if !visited_dir.insert((next_pos, dir)) {
+                    return None;
+                };
+                pos = next_pos;
+            }
+        }
+    }
 }
 
 fn parse_input(input: &str) -> (Map, (usize, usize)) {
@@ -34,61 +93,22 @@ fn parse_input(input: &str) -> (Map, (usize, usize)) {
 }
 
 pub fn level1(input: &str) -> usize {
-    #[derive(Debug)]
-    enum Dir {
-        North,
-        East,
-        South,
-        West,
-    }
-    let (map, mut pos) = parse_input(input);
-    let mut dir = Dir::North;
-    let mut visited: HashSet<_> = std::iter::once(pos).collect();
-    loop {
-        let next_pos = match dir {
-            Dir::North => {
-                let Some(y) = pos.1.checked_sub(1) else {
-                    return visited.len();
-                };
-                (pos.0, y)
-            }
-            Dir::East => {
-                let x = pos.0 + 1;
-                if x >= map.width {
-                    return visited.len();
-                }
-                (x, pos.1)
-            }
-            Dir::South => {
-                let y = pos.1 + 1;
-                if y >= map.height {
-                    return visited.len();
-                }
-                (pos.0, y)
-            }
-            Dir::West => {
-                let Some(x) = pos.0.checked_sub(1) else {
-                    return visited.len();
-                };
-                (x, pos.1)
-            }
-        };
-        if map.obstacles.contains(&next_pos) {
-            dir = match dir {
-                Dir::North => Dir::East,
-                Dir::East => Dir::South,
-                Dir::South => Dir::West,
-                Dir::West => Dir::North,
-            }
-        } else {
-            visited.insert(next_pos);
-            pos = next_pos;
-        }
-    }
+    let (map, pos) = parse_input(input);
+    map.steps_till_escape(pos).unwrap().len()
 }
 
 pub fn level2(input: &str) -> usize {
-    0
+    use rayon::prelude::*;
+    let (map, start_pos) = parse_input(input);
+    let visited = map.steps_till_escape(start_pos).unwrap();
+    visited
+        .into_par_iter()
+        .filter(|pos| {
+            let mut map = map.clone();
+            map.obstacles.insert(*pos);
+            map.steps_till_escape(start_pos).is_none()
+        })
+        .count()
 }
 
 #[cfg(test)]
@@ -104,6 +124,6 @@ mod test {
     #[test]
     fn level2_given_example() {
         let test_input = include_str!("./test_input/day6.txt");
-        assert_eq!(level2(test_input), 0)
+        assert_eq!(level2(test_input), 6)
     }
 }
