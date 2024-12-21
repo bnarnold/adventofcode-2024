140cff0b8726980bb5fc71507af556e71647108d
Bertram Arnold <bertram.arnold@gmail.com>
2024-12-21 09:53:45 +0100

    day 20 part 2 with visualizer

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -1,17 +1,29 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    io::stdout,
+    time::Duration,
+};
 
+use crossterm::{
+    cursor::{Hide, MoveTo},
+    event, execute,
+    style::{Color, PrintStyledContent, Stylize},
+    terminal::Clear,
+};
 use nom::combinator::opt;
 
 use crate::util::prelude::*;
 
 #[derive(Debug)]
 struct Map {
+    /// The positions of the path in order.
     fields: Vec<[u16; 2]>,
-    field_positions: HashMap<[u16; 2], usize>,
     width: u16,
     height: u16,
-    start_pos: [u16; 2],
-    end_pos: [u16; 2],
+    /// The positions grouped by the main diagonal, i.e. the difference of the coordinates.
+    main_diagonals: Vec<HashSet<usize>>,
+    /// The positions grouped by the off diagonal, i.e. the sum of the coordinates.
+    off_diagonals: Vec<HashSet<usize>>,
 }
 
 impl Map {
@@ -50,12 +62,11 @@ impl Map {
         let start_pos = start_pos.ok_or("no start")?;
         let end_pos = end_pos.ok_or("no end")?;
 
-        let mut field_positions = HashMap::new();
-        field_positions.insert(start_pos, 0);
+        let mut visited = HashSet::new();
         let mut fields = vec![start_pos];
 
         let mut pos = start_pos;
-        for steps in 1.. {
+        loop {
             let next_pos = [[0, -1], [1, 0], [0, 1], [-1, 0]]
                 .into_iter()
                 .flat_map(|delta| {
@@ -63,12 +74,11 @@ impl Map {
                         pos[0].checked_add_signed(delta[0])?,
                         pos[1].checked_add_signed(delta[1])?,
                     ];
-                    (field_set.contains(&new_pos) && !field_positions.contains_key(&new_pos))
-                        .then_some(new_pos)
+                    (field_set.contains(&new_pos) && !visited.contains(&new_pos)).then_some(new_pos)
                 })
                 .next()
                 .ok_or("no path")?;
-            field_positions.insert(next_pos, steps);
+            visited.insert(next_pos);
             fields.push(next_pos);
             if next_pos == end_pos {
                 break;
@@ -76,58 +86,122 @@ impl Map {
             pos = next_pos
         }
 
+        let mut main_diagonals = Vec::new();
+        let diagonal_count = (width + height - 1) as usize;
+        main_diagonals.resize_with(diagonal_count, HashSet::new);
+        let mut off_diagonals = Vec::new();
+        off_diagonals.resize_with(diagonal_count, HashSet::new);
+
+        for (i, &pos) in fields.iter().enumerate() {
+            main_diagonals[(width - pos[0] - 1 + pos[1]) as usize].insert(i);
+            off_diagonals[(pos[0] + pos[1]) as usize].insert(i);
+        }
+
         Ok(Self {
             fields,
-            field_positions,
             width,
             height,
-            start_pos,
-            end_pos,
+            main_diagonals,
+            off_diagonals,
         })
     }
 
-    fn cheat_savings(&self) -> Vec<usize> {
-        let mut result = Vec::new();
+    fn cheat_savings(
+        &self,
+        cheat_distance: usize,
+        min_gain: usize,
+    ) -> impl Iterator<Item = usize> + '_ {
+        let mut plot = std::env::var_os("PLOT").map(|_| stdout());
 
-        for (i, pos) in self.fields.iter().enumerate() {
-            let cheat_start_score = i;
-            let neighbors = |pos: [u16; 2]| {
-                [[0, -1], [1, 0], [0, 1], [-1, 0]]
-                    .into_iter()
-                    .flat_map(move |delta| {
-                        let new_pos = [
-                            pos[0].checked_add_signed(delta[0])?,
-                            pos[1].checked_add_signed(delta[1])?,
-                        ];
-                        (new_pos[0] < self.width && new_pos[1] < self.height).then_some(new_pos)
-                    })
-            };
-            result.extend(
-                neighbors(*pos)
-                    .filter(|neighbor| !self.field_positions.contains_key(neighbor))
-                    .flat_map(neighbors)
-                    .flat_map(|double_neighbor| {
-                        let cheat_end_score = self.field_positions.get(&double_neighbor)?;
-                        let difference = cheat_end_score.checked_sub(cheat_start_score)?;
-                        difference.checked_sub(2)
-                    }),
-            );
+        if let Some(ref mut stdout) = plot {
+            execute!(stdout, Clear(crossterm::terminal::ClearType::All), Hide).unwrap();
         }
+        let main_diagonals = (0..self.main_diagonals.len())
+            .map(|diag| {
+                let min = diag.saturating_sub(cheat_distance);
+                let max = (diag + cheat_distance).min(self.main_diagonals.len() - 1);
+                self.main_diagonals[min..=max]
+                    .iter()
+                    .fold(HashSet::<usize>::new(), |acc, steps| &acc | steps)
+            })
+            .collect_vec();
+        let off_diagonals = (0..self.off_diagonals.len())
+            .map(|diag| {
+                let min = diag.saturating_sub(cheat_distance);
+                let max = (diag + cheat_distance).min(self.off_diagonals.len() - 1);
+                self.off_diagonals[min..=max]
+                    .iter()
+                    .fold(HashSet::<usize>::new(), |acc, steps| &acc | steps)
+            })
+            .collect_vec();
+        self.fields.iter().enumerate().flat_map(move |(step, pos)| {
+            let main_diagonal = (self.width - 1 - pos[0] + pos[1]) as usize;
+            let off_diagonal = (pos[0] + pos[1]) as usize;
 
-        result
+            let reachable_on_main = &main_diagonals[main_diagonal];
+            let reachable_on_off = &off_diagonals[off_diagonal];
+
+            if let Some(ref mut stdout) = plot {
+                for (plot_step, plot_pos) in self.fields.iter().copied().enumerate() {
+                    let color = match (
+                        &plot_pos == pos,
+                        reachable_on_main.contains(&plot_step),
+                        reachable_on_off.contains(&plot_step),
+                    ) {
+                        (true, _, _) => Color::Blue,
+                        (_, true, false) => Color::Red,
+                        (_, false, true) => Color::Green,
+                        (_, false, false) => Color::Grey,
+                        (_, true, true) => {
+                            let cheat_distance =
+                                pos[0].abs_diff(plot_pos[0]) + pos[1].abs_diff(plot_pos[1]);
+                            if step + cheat_distance as usize + min_gain < plot_step {
+                                Color::Yellow
+                            } else {
+                                let gain = plot_step.saturating_sub(step + cheat_distance as usize);
+                                let scaled = (64 + (128 * gain) / min_gain) as u8;
+
+                                Color::Rgb {
+                                    r: scaled,
+                                    g: scaled,
+                                    b: scaled,
+                                }
+                            }
+                        }
+                    };
+                    execute!(
+                        stdout,
+                        MoveTo(plot_pos[0], plot_pos[1]),
+                        PrintStyledContent('▉'.stylize().with(color))
+                    )
+                    .unwrap();
+                }
+                execute!(stdout, MoveTo(0, self.height + 1)).unwrap();
+                std::thread::sleep(Duration::from_millis(20));
+            }
+            let reachable_fields = reachable_on_main & reachable_on_off;
+            reachable_fields
+                .into_iter()
+                .flat_map(move |reachable_step| {
+                    let path_distance = reachable_step.checked_sub(step)?;
+                    let target_pos = self.fields[reachable_step];
+                    let cheat_distance =
+                        pos[0].abs_diff(target_pos[0]) + pos[1].abs_diff(target_pos[1]);
+                    path_distance.checked_sub(cheat_distance as usize)
+                })
+                .filter(move |saving| *saving >= min_gain)
+        })
     }
 }
 
 pub fn level1(input: &str) -> usize {
     let map = Map::parse_input(input).expect("parse");
-    map.cheat_savings()
-        .into_iter()
-        .filter(|saving| *saving >= 100)
-        .count()
+    map.cheat_savings(2, 100).count()
 }
 
 pub fn level2(input: &str) -> usize {
-    0
+    let map = Map::parse_input(input).expect("parse");
+    map.cheat_savings(20, 100).count()
 }
 
 #[cfg(test)]
@@ -138,15 +212,18 @@ mod test {
     fn level1_given_example() {
         let test_input = include_str!("./test_input/day20.txt");
         let map = Map::parse_input(test_input).expect("parse");
-        let cheats = map.cheat_savings();
+        let cheats = map.cheat_savings(2, 1);
         let counts = cheats.into_iter().counts();
-        dbg!(&counts);
         assert_eq!(counts.get(&64).copied(), Some(1));
     }
 
     #[test]
     fn level2_given_example() {
         let test_input = include_str!("./test_input/day20.txt");
-        assert_eq!(level2(test_input), 0)
+        let map = Map::parse_input(test_input).expect("parse");
+        let cheats = map.cheat_savings(20, 50);
+        let counts = cheats.into_iter().counts();
+        assert_eq!(counts.get(&64).copied(), Some(19));
+        assert_eq!(counts.get(&76).copied(), Some(3));
     }
 }
